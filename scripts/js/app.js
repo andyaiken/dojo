989569cbb9308dd18fba52ea6190f3b2deda4c4d
Andy Aiken <aaiken@scottlogic.co.uk>
2018-12-06 16:59:21 +0000

    Map tile placement

--- scripts/js/app.js
+++ scripts/js/app.js
@@ -9477,8 +9477,8 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 /*
-Tile is: {
-    type: tile | token
+Map item is: {
+    type: tile | monster | pc
     x: number,
     y: number,
     width: number,
@@ -9489,46 +9489,74 @@ Tile is: {
 var MapPanel = function (_React$Component) {
     _inherits(MapPanel, _React$Component);
 
-    function MapPanel() {
+    function MapPanel(props) {
         _classCallCheck(this, MapPanel);
 
-        return _possibleConstructorReturn(this, (MapPanel.__proto__ || Object.getPrototypeOf(MapPanel)).apply(this, arguments));
+        var _this = _possibleConstructorReturn(this, (MapPanel.__proto__ || Object.getPrototypeOf(MapPanel)).call(this));
+
+        _this.state = {
+            map: props.map
+        };
+        return _this;
     }
 
     _createClass(MapPanel, [{
         key: "getMapDimensions",
         value: function getMapDimensions() {
-            var dimensions = {
-                minX: 0,
-                maxX: 0,
-                minY: 0,
-                maxY: 0,
-                width: function width() {
-                    return 1 + maxX - minX;
-                },
-                height: function height() {
-                    return 1 + maxY - minY;
-                }
-            };
+            var border = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 1;
 
-            this.props.map.items.forEach(function (i) {
-                dimensions.minX = Math.min(dimensions.minX, i.x);
-                dimensions.maxX = Math.max(dimensions.maxX, i.x);
-                dimensions.minY = Math.min(dimensions.minY, i.y);
-                dimensions.maxY = Math.max(dimensions.maxY, i.y);
+            var dimensions = null;
+
+            this.state.map.items.forEach(function (i) {
+                if (!dimensions) {
+                    dimensions = {
+                        minX: i.x,
+                        maxX: i.x,
+                        minY: i.y,
+                        maxY: i.y
+                    };
+                } else {
+                    dimensions.minX = Math.min(dimensions.minX, i.x);
+                    dimensions.maxX = Math.max(dimensions.maxX, i.x + i.width - 1);
+                    dimensions.minY = Math.min(dimensions.minY, i.y);
+                    dimensions.maxY = Math.max(dimensions.maxY, i.y + i.height - 1);
+                }
             });
+
+            // Apply the border
+            dimensions.minX -= border;
+            dimensions.maxX += border;
+            dimensions.minY -= border;
+            dimensions.maxY += border;
+
+            dimensions.width = 1 + dimensions.maxX - dimensions.minX;
+            dimensions.height = 1 + dimensions.maxY - dimensions.minY;
 
             return dimensions;
         }
     }, {
-        key: "getLocation",
-        value: function getLocation(x, y, width, height, mapDimensions) {
-            // TODO: Translate tile location to canvas location
+        key: "getSideLength",
+        value: function getSideLength() {
+            switch (this.props.mode) {
+                case "thumbnail":
+                    return 5;
+                case "edit":
+                case "combat":
+                    return 25;
+            }
+
+            return 0;
+        }
+    }, {
+        key: "getPosition",
+        value: function getPosition(x, y, width, height, mapDimensions) {
+            var sideLength = this.getSideLength();
+
             return {
-                left: 0,
-                top: 0,
-                width: "5px",
-                height: "5px"
+                left: "calc(" + sideLength + "px * " + (x - mapDimensions.minX) + ")",
+                top: "calc(" + sideLength + "px * " + (y - mapDimensions.minY) + ")",
+                width: "calc(" + sideLength + "px * " + width + ")",
+                height: "calc(" + sideLength + "px * " + height + ")"
             };
         }
     }, {
@@ -9537,76 +9565,113 @@ var MapPanel = function (_React$Component) {
             var _this2 = this;
 
             try {
-                // TODO: Find the dimensions of the map
-                var mapDimensions = this.getMapDimensions();
+                // TEMP
+                this.state.map.items = [{
+                    type: "tile",
+                    x: 0,
+                    y: 0,
+                    width: 10,
+                    height: 5
+                }, {
+                    type: "tile",
+                    x: 10,
+                    y: 0,
+                    width: 2,
+                    height: 10
+                }, {
+                    type: "monster",
+                    x: 2,
+                    y: 2,
+                    width: 2,
+                    height: 2
+                }, {
+                    type: "pc",
+                    x: 4,
+                    y: 2,
+                    width: 1,
+                    height: 1
+                }];
 
-                // TODO: Build the grid squares
+                var border = 2;
+                var mapDimensions = this.getMapDimensions(border);
+
+                // Draw the grid squares
                 var grid = [];
-                for (var x = mapDimensions.minX; x !== mapDimensions.maxX + 1; ++x) {
-                    for (var y = mapDimensions.minY; y !== mapDimensions.maxY + 1; ++y) {
-                        var loc = this.getLocation(x, y, 1, 1);
-                        grid.push(React.createElement(
-                            "div",
-                            { className: "grid-square", style: loc },
-                            x,
-                            ", ",
-                            y
-                        ));
+                if (this.props.mode === "edit") {
+                    for (var x = mapDimensions.minX; x !== mapDimensions.maxX + 1; ++x) {
+                        for (var y = mapDimensions.minY; y !== mapDimensions.maxY + 1; ++y) {
+                            var pos = this.getPosition(x, y, 1, 1, mapDimensions);
+                            grid.push(React.createElement("div", { className: "grid-square", style: pos }));
+                        }
                     }
                 }
 
-                var tiles = this.props.map.items.filter(function (i) {
+                // Draw the map tiles
+                var tiles = this.state.map.items.filter(function (i) {
                     return i.type === "tile";
                 }).map(function (i) {
-                    // TODO: Draw tile
-                    var loc = _this2.getLocation(i.x, i.y, i.width, i.height);
-                    return React.createElement(
-                        "div",
-                        { className: "tile" },
-                        i.type
-                    );
+                    var pos = _this2.getPosition(i.x, i.y, i.width, i.height, mapDimensions);
+                    return React.createElement("div", { className: "tile", style: pos });
                 });
 
-                var tokens = this.props.map.items.filter(function (i) {
-                    return i.type === "token";
-                }).map(function (i) {
-                    // TODO: Draw token
-                    var loc = _this2.getLocation(i.x, i.y, i.width, i.height);
-                    return React.createElement(
-                        "div",
-                        { className: "token" },
-                        i.type
-                    );
-                });
+                // Draw the tokens
+                var tokens = [];
+                if (this.props.mode !== "edit") {
+                    tokens = this.state.map.items.filter(function (i) {
+                        return i.type === "monster" || i.type === "pc";
+                    }).map(function (i) {
+                        var pos = _this2.getPosition(i.x, i.y, i.width, i.height, mapDimensions);
+                        return React.createElement("div", { className: "token " + i.type, style: pos });
+                    });
+                }
 
+                // Draw tools
                 var tools = null;
                 switch (this.props.mode) {
-                    case "view":
-                        // No tools in view mode
+                    case "thumbnail":
+                        // No tools in thumbnail mode
                         break;
                     case "edit":
                         // TODO: Draw tools
                         // If no selection: tiles you can drag onto the map
                         // If selection: allow editing the selection
+                        tools = React.createElement(
+                            "div",
+                            { className: "tools" },
+                            React.createElement(
+                                "div",
+                                { className: "heading" },
+                                "tools"
+                            )
+                        );
                         break;
                     case "combat":
                         // TODO: Draw tools
                         // If no selection: combatants that aren't on the map
                         // If selection: allow editing the selection
+                        tools = React.createElement(
+                            "div",
+                            { className: "tools" },
+                            React.createElement(
+                                "div",
+                                { className: "heading" },
+                                "tools"
+                            )
+                        );
                         break;
                 }
 
                 return React.createElement(
                     "div",
                     { className: "map-panel " + this.props.mode },
+                    tools,
                     React.createElement(
                         "div",
-                        { className: "grid" },
+                        { className: "grid", style: { height: this.getSideLength() * mapDimensions.height + "px" } },
                         grid,
                         tiles,
                         tokens
-                    ),
-                    tools
+                    )
                 );
             } catch (e) {
                 console.error(e);
--- scripts/jsx/panels/map-panel.jsx
+++ scripts/jsx/panels/map-panel.jsx
@@ -1,6 +1,6 @@
 /*
-Tile is: {
-    type: tile | token
+Map item is: {
+    type: tile | monster | pc
     x: number,
     y: number,
     width: number,
@@ -9,97 +9,173 @@ Tile is: {
 */
 
 class MapPanel extends React.Component {
-    getMapDimensions() {
-        var dimensions = {
-            minX: 0,
-            maxX: 0,
-            minY: 0,
-            maxY: 0,
-            width: () => 1 + maxX - minX,
-            height: () => 1 + maxY - minY
+    constructor(props) {
+        super();
+        this.state = {
+            map: props.map
         };
+    }
 
-        this.props.map.items.forEach(i => {
-            dimensions.minX = Math.min(dimensions.minX, i.x);
-            dimensions.maxX = Math.max(dimensions.maxX, i.x);
-            dimensions.minY = Math.min(dimensions.minY, i.y);
-            dimensions.maxY = Math.max(dimensions.maxY, i.y);
+    getMapDimensions(border = 1) {
+        var dimensions = null;
+
+        this.state.map.items.forEach(i => {
+            if (!dimensions) {
+                dimensions = {
+                    minX: i.x,
+                    maxX: i.x,
+                    minY: i.y,
+                    maxY: i.y
+                };
+            } else {
+                dimensions.minX = Math.min(dimensions.minX, i.x);
+                dimensions.maxX = Math.max(dimensions.maxX, i.x + i.width - 1);
+                dimensions.minY = Math.min(dimensions.minY, i.y);
+                dimensions.maxY = Math.max(dimensions.maxY, i.y + i.height - 1);
+            }
         });
+
+        // Apply the border
+        dimensions.minX -= border;
+        dimensions.maxX += border;
+        dimensions.minY -= border;
+        dimensions.maxY += border;
+
+        dimensions.width = 1 + dimensions.maxX - dimensions.minX;
+        dimensions.height = 1 + dimensions.maxY - dimensions.minY;
 
         return dimensions;
     }
 
-    getLocation(x, y, width, height, mapDimensions) {
-        // TODO: Translate tile location to canvas location
+    getSideLength() {
+        switch (this.props.mode) {
+            case "thumbnail":
+                return 5;
+            case "edit":
+            case "combat":
+                return 25;
+        }
+
+        return 0;
+    }
+
+    getPosition(x, y, width, height, mapDimensions) {
+        var sideLength = this.getSideLength();
+
         return {
-            left: 0,
-            top: 0,
-            width: "5px",
-            height: "5px"
+            left: "calc(" + sideLength + "px * " + (x - mapDimensions.minX) + ")",
+            top: "calc(" + sideLength + "px * " + (y - mapDimensions.minY) + ")",
+            width: "calc(" + sideLength + "px * " + width + ")",
+            height: "calc(" + sideLength + "px * " + height + ")"
         };
     }
 
     render() {
         try {
-            // TODO: Find the dimensions of the map
-            var mapDimensions = this.getMapDimensions();
+            // TEMP
+            this.state.map.items = [
+                {
+                    type: "tile",
+                    x: 0,
+                    y: 0,
+                    width: 10,
+                    height: 5,
+                }, {
+                    type: "tile",
+                    x: 10,
+                    y: 0,
+                    width: 2,
+                    height: 10,
+                }, {
+                    type: "monster",
+                    x: 2,
+                    y: 2,
+                    width: 2,
+                    height: 2,
+                }, {
+                    type: "pc",
+                    x: 4,
+                    y: 2,
+                    width: 1,
+                    height: 1,
+                }
+            ];
 
-            // TODO: Build the grid squares
+            var border = 2;
+            var mapDimensions = this.getMapDimensions(border);
+
+            // Draw the grid squares
             var grid = [];
-            for (var x = mapDimensions.minX; x !== mapDimensions.maxX + 1; ++x) {
-                for (var y = mapDimensions.minY; y !== mapDimensions.maxY + 1; ++y) {
-                    var loc = this.getLocation(x, y, 1, 1);
-                    grid.push(
-                        <div className="grid-square" style={loc}>{x}, {y}</div>
-                    );
+            if (this.props.mode === "edit") {
+                for (var x = mapDimensions.minX; x !== mapDimensions.maxX + 1; ++x) {
+                    for (var y = mapDimensions.minY; y !== mapDimensions.maxY + 1; ++y) {
+                        var pos = this.getPosition(x, y, 1, 1, mapDimensions);
+                        grid.push(
+                            <div className="grid-square" style={pos}></div>
+                        );
+                    }
                 }
             }
 
-            var tiles = this.props.map.items
+            // Draw the map tiles
+            var tiles = this.state.map.items
                 .filter(i => i.type === "tile")
                 .map(i => {
-                    // TODO: Draw tile
-                    var loc = this.getLocation(i.x, i.y, i.width, i.height);
+                    var pos = this.getPosition(i.x, i.y, i.width, i.height, mapDimensions);
                     return (
-                        <div className="tile">{i.type}</div>
+                        <div className="tile" style={pos}></div>
                     );
                 });
 
-            var tokens = this.props.map.items
-                .filter(i => i.type === "token")
+            // Draw the tokens
+            var tokens = [];
+            if (this.props.mode !== "edit") {
+                tokens = this.state.map.items
+                .filter(i => (i.type === "monster") || (i.type === "pc"))
                 .map(i => {
-                    // TODO: Draw token
-                    var loc = this.getLocation(i.x, i.y, i.width, i.height);
+                    var pos = this.getPosition(i.x, i.y, i.width, i.height, mapDimensions);
                     return (
-                        <div className="token">{i.type}</div>
+                        <div className={"token " + i.type} style={pos}></div>
                     );
                 });
+            }
 
+            // Draw tools
             var tools = null;
             switch (this.props.mode) {
-                case "view":
-                    // No tools in view mode
+                case "thumbnail":
+                    // No tools in thumbnail mode
                     break
                 case "edit":
                     // TODO: Draw tools
                     // If no selection: tiles you can drag onto the map
                     // If selection: allow editing the selection
+                    tools = (
+                        <div className="tools">
+                            <div className="heading">tools</div>
+                        </div>
+                    )
                     break
                 case "combat":
                     // TODO: Draw tools
                     // If no selection: combatants that aren't on the map
                     // If selection: allow editing the selection
+                    tools = (
+                        <div className="tools">
+                            <div className="heading">tools</div>
+                        </div>
+                    )
                     break
             }
 
             return (
                 <div className={"map-panel " + this.props.mode}>
-                    <div className="grid">
+                    {tools}
+                    <div className="grid" style={{ height: (this.getSideLength() * mapDimensions.height) + "px" }}>
                         {grid}
                         {tiles}
                         {tokens}
                     </div>
-                    {tools}
                 </div>
             );
         } catch (e) {
