6c4ab559253e504746f104fd7de57ad8c2ba3c26
Andy Aiken <aaiken@scottlogic.co.uk>
2019-10-09 12:20:15 +0100

    #63

--- src/components/tools/dm/die-roller-module.tsx
+++ src/components/tools/dm/die-roller-module.tsx
@@ -4,6 +4,8 @@ import Utils from '../../../utils/utils';
 
 import Selector from '../../controls/selector';
 import Spin from '../../controls/spin';
+import ControlRow from '../../controls/control-row';
+import Checkbox from '../../controls/checkbox';
 
 // tslint:disable-next-line:no-empty-interface
 interface Props {
@@ -13,6 +15,7 @@ interface Props {
 interface State {
     dice: string;
     count: number;
+    options: string[];
     rolls: number[] | null;
     result: number | null;
 }
@@ -24,6 +27,7 @@ export default class DieRollerModule extends React.Component<Props, State> {
         this.state = {
             dice: '20',
             count: 1,
+            options: [],
             rolls: null,
             result: null
         };
@@ -41,17 +45,52 @@ export default class DieRollerModule extends React.Component<Props, State> {
         });
     }
 
+    private toggleOption(option: string) {
+        if (this.state.options.includes(option)) {
+            // Remove option
+            const index = this.state.options.indexOf(option);
+            this.state.options.splice(index, 1);
+        } else {
+            // Add option
+            this.state.options.push(option);
+            // Make sure we don't have both advantage and disadvantage
+            if (this.state.options.includes('advantage') && this.state.options.includes('disadvantage')) {
+                const index = this.state.options.indexOf(option === 'advantage' ? 'disadvantage' : 'advantage');
+                this.state.options.splice(index, 1);
+            }
+        }
+        this.setState({
+            options: this.state.options
+        });
+    }
+
     private roll() {
         const sides = parseInt(this.state.dice, 10);
 
         const rolls: number[] = [];
-        for (let n = 0; n !== this.state.count; ++n) {
+        let count = this.state.count;
+        if (this.state.options.includes('advantage') || this.state.options.includes('disadvantage')) {
+            count = 2;
+        }
+        for (let n = 0; n !== count; ++n) {
             rolls.push(Utils.dieRoll(sides));
         }
         rolls.sort((a, b) => a - b);
 
         let result = 0;
-        rolls.forEach(roll => result += roll);
+        if (this.state.options.includes('advantage')) {
+            result = Math.max(...rolls);
+        } else if (this.state.options.includes('disadvantage')) {
+            result = Math.min(...rolls);
+        } else {
+            rolls.forEach(roll => result += roll);
+            if (this.state.options.includes('drop lowest')) {
+                result -= Math.min(...rolls);
+            }
+            if (this.state.options.includes('drop highest')) {
+                result -= Math.max(...rolls);
+            }
+        }
 
         this.setState({
             rolls: rolls,
@@ -91,8 +130,49 @@ export default class DieRollerModule extends React.Component<Props, State> {
             }
         ];
 
+        let optionsSection = null;
+        if ((this.state.dice === '20') && (this.state.count === 1)) {
+            optionsSection = (
+                <ControlRow
+                    controls={[
+                        <Checkbox
+                            key='advantage'
+                            label='advantage'
+                            checked={this.state.options.includes('advantage')}
+                            changeValue={value => this.toggleOption('advantage')}
+                        />,
+                        <Checkbox
+                            key='disadvantage'
+                            label='disadvantage'
+                            checked={this.state.options.includes('disadvantage')}
+                            changeValue={value => this.toggleOption('disadvantage')}
+                        />
+                    ]}
+                />
+            );
+        } else if (this.state.count > 1) {
+            optionsSection = (
+                <ControlRow
+                    controls={[
+                        <Checkbox
+                            key='drop-lowest'
+                            label='drop lowest'
+                            checked={this.state.options.includes('drop lowest')}
+                            changeValue={value => this.toggleOption('drop lowest')}
+                        />,
+                        <Checkbox
+                            key='drop-highest'
+                            label='drop highest'
+                            checked={this.state.options.includes('drop highest')}
+                            changeValue={value => this.toggleOption('drop highest')}
+                        />
+                    ]}
+                />
+            );
+        }
+
         let rollsSection = null;
-        if (this.state.rolls !== null) {
+        if ((this.state.rolls !== null) && (this.state.rolls.length > 1)) {
             rollsSection = (
                 <div className='section die-rolls'>{this.state.rolls.join(', ')}</div>
             );
@@ -113,13 +193,15 @@ export default class DieRollerModule extends React.Component<Props, State> {
                     selectedID={this.state.dice}
                     select={optionID => this.setDice(optionID)}
                 />
-                <div className='subheading'>number to roll</div>
+                <div className='subheading'>number of dice to roll</div>
                 <Spin
                     source={this.state}
                     name='count'
                     display={count => count + 'd' + this.state.dice}
                     nudgeValue={delta => this.nudgeCount(delta)}
                 />
+                {optionsSection ? <div className='subheading'>options</div> : null}
+                {optionsSection}
                 <div className='divider' />
                 <button onClick={() => this.roll()}>roll dice</button>
                 {rollsSection}
