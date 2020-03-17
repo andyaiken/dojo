cd8eb0a318e160268766882cef2766d9da46b05f
Andy Aiken <aaiken@scottlogic.com>
2020-03-17 15:27:58 +0000

    Updates

--- precache-manifest.20599704acf1879a7d1c53075efda58f.js
+++ precache-manifest.20599704acf1879a7d1c53075efda58f.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1256dee85514a3749f9278a4cdb148ec",
+    "revision": "af8da04cc28695b7bc8765320ca9212c",
     "url": "/dojo/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/dojo/static/css/2.8bc3b147.chunk.css"
   },
   {
-    "revision": "3fb712571a1d2a9383d5",
-    "url": "/dojo/static/css/main.41b8cd37.chunk.css"
+    "revision": "36d5dd80243deaa9cbe4",
+    "url": "/dojo/static/css/main.fb06ad15.chunk.css"
   },
   {
     "revision": "16b0e18fdb52a6bf890a",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/dojo/static/js/2.5843f55f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3fb712571a1d2a9383d5",
-    "url": "/dojo/static/js/main.fb9cfce2.chunk.js"
+    "revision": "36d5dd80243deaa9cbe4",
+    "url": "/dojo/static/js/main.763349a1.chunk.js"
   },
   {
     "revision": "6963a6ac294489a91911",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dojo/precache-manifest.0c412b55a0e9ef5766bfd8f6cd8abcf7.js"
+  "/dojo/precache-manifest.20599704acf1879a7d1c53075efda58f.js"
 );
 
 self.addEventListener('message', (event) => {
