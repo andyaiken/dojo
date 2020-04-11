a6bcee039f7d5a3f5b8ac35f8039d1c340e3eed5
Andy Aiken <aaiken@scottlogic.com>
2020-04-12 00:17:20 +0100

    Updates

---
+++ precache-manifest.c030bb3f7ea564aceab6561f8ed6d9c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5211647ef3a5f2870f1d5ad7a1f4c7de",
+    "url": "/dojo/index.html"
+  },
+  {
+    "revision": "f02d3a565a11b54e532d",
+    "url": "/dojo/static/css/2.43dba257.chunk.css"
+  },
+  {
+    "revision": "32a25218ecfde18b31a2",
+    "url": "/dojo/static/css/main.82e440b0.chunk.css"
+  },
+  {
+    "revision": "f02d3a565a11b54e532d",
+    "url": "/dojo/static/js/2.e418c1c4.chunk.js"
+  },
+  {
+    "revision": "8457097bb962228e210b2c7b3d5fc1fb",
+    "url": "/dojo/static/js/2.e418c1c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32a25218ecfde18b31a2",
+    "url": "/dojo/static/js/main.c327fc15.chunk.js"
+  },
+  {
+    "revision": "9c7a795e8c37a0711e48",
+    "url": "/dojo/static/js/runtime-main.65d24c8c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dojo/precache-manifest.d4dd4105255282aeea2e0afe82728fb9.js"
+  "/dojo/precache-manifest.c030bb3f7ea564aceab6561f8ed6d9c1.js"
 );
 
 self.addEventListener('message', (event) => {
