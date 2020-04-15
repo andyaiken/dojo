80432ef9e1a9666aaa0ee3d1b58c4ea9eae02f6f
Andy Aiken <aaiken@scottlogic.com>
2020-04-15 16:29:18 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dojo/precache-manifest.01ab8544e603f942f5877d28941768ac.js"
+  "/dojo/precache-manifest.be45d89a1641e5d7d944f158fd65039f.js"
 );
 
 self.addEventListener('message', (event) => {
