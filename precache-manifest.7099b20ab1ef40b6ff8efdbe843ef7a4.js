8ff560f4dbbf3e23907c27f71e9084666a39445c
Andy Aiken <aaiken@scottlogic.com>
2020-09-06 20:53:02 +0100

    Updates

---
+++ precache-manifest.7099b20ab1ef40b6ff8efdbe843ef7a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcdc64de3a9be8f1f2d43a90352a74c2",
+    "url": "/dojo/index.html"
+  },
+  {
+    "revision": "4884b5bf09e7909f94af",
+    "url": "/dojo/static/css/2.542dbdfd.chunk.css"
+  },
+  {
+    "revision": "34239189a939b3b30344",
+    "url": "/dojo/static/css/main.3b8d01b7.chunk.css"
+  },
+  {
+    "revision": "4884b5bf09e7909f94af",
+    "url": "/dojo/static/js/2.73a7c67a.chunk.js"
+  },
+  {
+    "revision": "8457097bb962228e210b2c7b3d5fc1fb",
+    "url": "/dojo/static/js/2.73a7c67a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34239189a939b3b30344",
+    "url": "/dojo/static/js/main.1c1bcd1e.chunk.js"
+  },
+  {
+    "revision": "fcc6865aa2a03536f08a",
+    "url": "/dojo/static/js/runtime-main.3002b322.js"
+  }
+]);
